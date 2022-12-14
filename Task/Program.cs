5238d6ce2a7bafe443669fd87cd042202d4b45b5
Denis <denisalkaev1986@gmail.com>
2022-12-14 16:59:18 +0300

    Added the solution for task

---
+++ Task/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+string[] CreateArray(string text)
+{
+    char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+    string[] result = text.Split(delimiterChars);
+    result = result.Where(x => x.Length <= 3).ToArray();
+
+    return result;
+}
+
+Console.Write("Input text: ");
+string N = Console.ReadLine()!;
+string[] res = CreateArray(N);
+Console.WriteLine($"Result: {string.Join(", ", res)}");
